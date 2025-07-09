8c2519a504d6b25e3ebb541ff124f3f1c81aa6da
sanaipei <lenapunyasanaipei@gmail.com>
2025-07-09 05:38:46 +0300

    Added UserProfile model and admin registration

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    verification_token = models.CharField(max_length=50, blank=True)
+    is_verified = models.BooleanField(default=False)
+
+    def generate_verification_token(self):
+        self.verification_token = uuid.uuid4().hex[:50]
+        self.save()
+        return self.verification_token
